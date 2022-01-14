8b9c935d6eecdd75d35209dd77e222874b938b4d
Stetsko Roman <asgzxba@mail.ru>
2022-01-14 21:21:15 +0300

    All Java Classes no redacting

---
+++ src/com/epam/training/student_Roman_Stecko/JavaClasses/Abiturients.java
@@ -0,0 +1,100 @@
+package com.epam.training.student_Roman_Stecko.JavaClasses;
+
+public class Abiturients {
+
+    private long id;
+    private String surname;
+    private String name;
+    private String patronymic;
+    private String addres;
+    private String phoneNumber;
+    private int[] estimation;
+
+//getters setters
+    public long getId() {
+        return id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int[] getEstimation() {
+        return estimation;
+    }
+
+    public void setEstimation(int[] estimation) {
+        this.estimation = estimation;
+    }
+
+    public Abiturients(long id, String surname, String name, String patronymic, String addres, String phoneNumber, int[] estimation) {
+        this.id = id;
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.addres = addres;
+        this.phoneNumber = phoneNumber;
+        this.estimation = estimation;
+    }
+
+
+    private String printArray (int[] estimation) {
+        String numbers = "";
+        for (int num: estimation){
+            numbers += num + "  ";
+        }
+        return numbers;
+    }
+
+    public void printInfo(){
+        System.out.println(id + ",  " + surname + " " + name + " " + patronymic + ":  " + addres + ",  " + phoneNumber + ". Estimations: " + printArray(estimation));
+    }
+    public double avg (int[] estimation){
+        int result = 0;
+        for (int num: estimation){
+            result += num;
+        }
+        return (double)result/(double)estimation.length;
+    }
+
+}
---
+++ src/com/epam/training/student_Roman_Stecko/JavaClasses/Calculate.java
@@ -0,0 +1,56 @@
+package com.epam.training.student_Roman_Stecko.JavaClasses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculate {
+    public static void main(String[] args) {
+
+        List<Abiturients> arrayAbiturients = new ArrayList<>();
+
+        arrayAbiturients.add(new Abiturients(111, "Sidorov", "Taras", "Ivanovich", "Minsk, Surganova 35/5", "Phone: 111-111",
+                new int[]{4, 2, 4, 5}));
+        arrayAbiturients.add(new Abiturients(222, "Petrov", "Marat", "Petrovich", "Minsk, Surganova 35/7", "Phone: 222-222",
+                new int[]{4, 6, 5, 7}));
+        arrayAbiturients.add(new Abiturients(333, "Tarasevich", "Gennadiy", "Tihonovich", "Minsk, Kolasa 19", "Phone: 333-333",
+                new int[]{6, 9, 7, 9}));
+        arrayAbiturients.add(new Abiturients(444, "Bur", "Olga", "Tihonovna", "Minsk, Kolasa 57", "Phone: 444-444",
+                new int[]{2, 5, 4, 7}));
+        arrayAbiturients.add(new Abiturients(555, "Kravec", "Mariya", "Fedorovna", "Minsk, Kolasa 55/2", "Phone: 555-555",
+                new int[]{7, 8, 9, 9}));
+
+        System.out.println("list of abiturients");
+        for (Abiturients ab : arrayAbiturients)
+            ab.printInfo();
+
+        System.out.println("\nList of abiturients with unsatisfactory estimations");
+        int neud = 4;
+        for (Abiturients ab : arrayAbiturients) {
+            for (int num : ab.getEstimation()) {
+                if (num < neud) {
+                    ab.printInfo();
+                }
+            }
+        }
+
+        int zadannoe = 7;
+        System.out.println("\nList of abiturients with average grades above the set " + zadannoe);
+        for (Abiturients ab : arrayAbiturients) {
+            if (ab.avg(ab.getEstimation()) > zadannoe) {
+                ab.printInfo();
+            }
+        }
+
+        System.out.println("\nList of abiturients whose grades are above than average ");
+        Abiturients maxAb;
+        for (int i = 0; i < arrayAbiturients.size() - 1; i++) {
+            int tempi = i + 1;
+            if (arrayAbiturients.get(i).avg(arrayAbiturients.get(i).getEstimation()) < arrayAbiturients
+                    .get(tempi).avg(arrayAbiturients.get(tempi).getEstimation())) {
+                maxAb = arrayAbiturients.get(tempi);
+                maxAb.printInfo();
+
+            }
+        }
+    }
+}
